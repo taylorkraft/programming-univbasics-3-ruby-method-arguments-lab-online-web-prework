d719cebf4013867a8c9644eea9f780364b402b07
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-18 17:03:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction_with_language(name, language)
   puts Hello, my name is #{name} and I'm learning to code in #{language}
 end
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   puts Hello, my name is #{name} and I'm learning to code in #{language}
 end
 
